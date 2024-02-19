987604dddb425c73f904209f1c81f9cdc4ced101
Enrique Fdez <efernandezcostas@danielcastelao.org>
2024-02-19 10:32:26 +0100

    SegundoCommit_NuevaRama

--- src/ramas.java
+++ src/ramas.java
@@ -3,5 +3,6 @@ public class ramas {
         System.out.println("Creado");
         System.out.println("PrimerCommit_Main");
         System.out.println("PrimerCommit_NuevaRama");
+        System.out.println("SegundoCommit_NuevaRama");
     }
 }
